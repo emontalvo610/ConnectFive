a5b89fa7b265405da3b4c08397579fe590fd7791
Einar Montalvo <v-eimontalvo@microsoft.com>
2024-03-16 18:40:21 -0700

    [CF-3] Complete the backend with dotnet and SignalR

---
+++ SignalrProject/SignalrHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+public class SignalrHub : Hub
+{
+    public async Task makeMove(object data)
+    {
+        await Clients.All.SendAsync("moveMade", data);
+    }
+
+    public async Task resetGame(object data)
+    {
+        await Clients.All.SendAsync("gameReset", data);
+    }
+}
